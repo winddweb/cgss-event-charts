e3cfb6e7a11a2bf9c2c53622b096bf43c39a3eb7
Yifeng <windd@yifengs.local>
2016-05-04 03:09:51 -0700

    Initial commit

---
+++ draw_charts.py
@@ -0,0 +1,97 @@
+#! /usr/local/bin/python3
+
+import csv
+import sys
+import re
+import plotly.plotly as py
+import plotly.offline as pyof
+import plotly.graph_objs as go
+
+x_values = []
+y_values_2 = []
+y_values_10 = []
+y_values_20 = []
+y_values_50 = []
+y_values_100 = []
+chart_type = ""
+
+def append_matched(match, y_list):
+    if match:
+        score = match.group().replace('：','').replace('（', '').replace(':', '').replace('p', '')
+
+        y_list.append(score)
+
+with open(sys.argv[1], 'r') as f:
+    reader = csv.reader(f, delimiter=',')
+
+    for row in reader:
+        t_id, t_date, t_text = row
+
+        t_text = t_text.split('\n')
+
+        if 'deresute_border' in sys.argv[1]:
+            chart_type = "Current Score"
+            if '最終結果' in t_text[0]:
+                title, r_2k, r_10k, r_20k, r_50k, r_100k, _, link = t_text
+            else:
+                title, r_2k, r_10k, r_20k, r_50k, r_100k, _, date, link = t_text
+
+            score_regex = r"：\d+（"
+
+        if 'cindere_border' in sys.argv[1]:
+            chart_type = "Predicted Score"
+            title, date, r_2k, r_10k, r_20k, r_50k, r_100k, _ = t_text
+            score_regex = r":\d+p"
+
+        match_2 = re.search(score_regex, r_2k)
+        match_10 = re.search(score_regex, r_10k)
+        match_20 = re.search(score_regex, r_20k)
+        match_50 = re.search(score_regex, r_50k)
+        match_100 = re.search(score_regex, r_100k)
+
+        x_values.append(t_date)
+        append_matched(match_2, y_values_2)
+        append_matched(match_10, y_values_10)
+        append_matched(match_20, y_values_20)
+        append_matched(match_50, y_values_50)
+        append_matched(match_100, y_values_100)
+
+
+    trace_2 = go.Scatter(
+        x = x_values,
+        y = y_values_2,
+        name = "2000"
+    )
+
+    trace_10 = go.Scatter(
+        x = x_values,
+        y = y_values_10,
+        name = "10000"
+    )
+    trace_20 = go.Scatter(
+        x = x_values,
+        y = y_values_20,
+        name = "20000"
+    )
+    trace_50 = go.Scatter(
+        x = x_values,
+        y = y_values_50,
+        name = "50000"
+    )
+    trace_100 = go.Scatter(
+        x = x_values,
+        y = y_values_100,
+        name = "100000"
+    )
+
+    data = [trace_2, trace_10, trace_20, trace_50, trace_100]
+
+    layout = dict(title = 'Event Score Trending @ CGSS ' + chart_type,
+                  xaxis = dict(title = 'Date'),
+                  yaxis = dict(title = 'Scores'),
+                  )
+
+    fig = dict(data=data, layout=layout)
+
+    # py.plot(fig, filename=chart_type + 'multiple-line') # save online on plot.ly, account required
+    pyof.plot(fig, filename='html_charts/' + chart_type + 'multiple-line.html')
---
+++ get_tweet_data.py
@@ -0,0 +1,97 @@
+#! /usr/local/bin/python3
+
+import sys
+import csv
+from datetime import datetime
+import tweepy
+from auth_setting import auth_setting
+
+# TODO: avoid twitter api rate limitation
+
+def get_all_tweets(screen_name):
+
+    auth = tweepy.OAuthHandler(auth_setting["consumer_key"], auth_setting["consumer_secret"])
+    auth.set_access_token(auth_setting["access_token"], auth_setting["access_token_secret"])
+
+    api = tweepy.API(auth)
+
+    max_number = 400
+    min_date = datetime(2016, 4, 29, 0, 00)
+    max_date = datetime.today()
+
+    # min_date = datetime(2016, 4, 20, 0, 00)
+    # max_date = datetime(2016, 4, 29, 0, 00)
+
+    # stream listener
+    # class MyStreamListener(tweepy.StreamListener):
+
+    #     def on_status(self, status):
+    #         print(status.text)
+
+    # myStreamListener = MyStreamListener()
+    # myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+
+    # myStream.filter(track=['デレステ'])
+
+    # your own timeline
+    # public_tweets = api.home_timeline()
+
+    def stop_get_tweets(alltweets):
+        stop = True
+
+        # continue_get = len(alltweets) < max_number
+
+        stop = alltweets[-1].created_at < min_date
+
+        return stop
+
+    alltweets = []
+
+    # new_tweets = api.user_timeline(screen_name = screen_name, count=200)
+
+    # alltweets.extend(new_tweets)
+
+    # oldest = alltweets[-1].id - 1
+    # oldest_date = alltweets[-1].created_at
+
+    oldest = None
+
+    while True:
+        if oldest is None:
+            new_tweets = api.user_timeline(screen_name = screen_name, count=200)
+        else:
+            new_tweets = api.user_timeline(screen_name = screen_name, count=200,max_id=oldest)
+
+            print("tweets from: %s" % oldest_date)
+
+        alltweets.extend(new_tweets)
+
+        oldest = alltweets[-1].id - 1
+
+        oldest_date = alltweets[-1].created_at
+
+        print("%s tweets loaded" % len(alltweets))
+
+        if stop_get_tweets(alltweets):
+
+            alltweets = [tweet for tweet in alltweets if min_date < tweet.created_at < max_date]
+            print("%s of tweets recorded" % len(alltweets))
+            break
+
+    # outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
+
+    outtweets = [[tweet.id_str, tweet.created_at, tweet.text] for tweet in alltweets]
+
+
+    with open('%s_tweets.csv' % screen_name, 'w') as f:
+            writer = csv.writer(f, delimiter=',')
+            # writer.writerow(["id","created_at","text"])
+            writer.writerows(outtweets)
+
+    pass
+
+if __name__ == '__main__':
+    #pass in the username of the account you want to download
+    screen_name = sys.argv[1] if len(sys.argv) > 1 else "deresute_border"
+    get_all_tweets(screen_name)
+

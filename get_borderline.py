5724d1af72238621e84b3f8b2dd849d45c134643
Yifeng <windd@yifengs.local>
2016-05-05 05:40:28 -0700

    Added a script to run get data and draw charts in one command

---
+++ get_borderline.py
@@ -0,0 +1,26 @@
+#! /usr/local/bin/python3
+
+import sys
+import sh
+
+
+option = sys.argv[1] # predict or border
+
+get_tweet = sh.Command('/Users/windd/Documents/develop/@py/cgss-event-charts/get_tweet_data.py')
+
+draw_chart = sh.Command('./draw_charts.py')
+
+if option == 'border':
+    # ./get_tweet_data.py deresute_border
+    # ./draw_charts.py deresute_border_tweets.csv
+    output = get_tweet('deresute_border')
+    print(output)
+    draw_chart('deresute_border_tweets.csv')
+elif option == 'predict':
+    # ./get_tweet_data.py cindere_border
+    # ./draw_charts.py cindere_border_tweets.csv
+    output = get_tweet('cindere_border')
+    print(output)
+    draw_chart('cindere_border_tweets.csv')
+else:
+    print('Please give "predict" or "border" as the first argument')

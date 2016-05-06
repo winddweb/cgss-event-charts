55b1c87e204a011b29df9cb52ba113ebc45d4fac
Yifeng <winddweb@gmail.com>
2016-05-06 16:14:25 -0700

    Fixed when max date is today, the newest tweets missing because of the time difference between JP and USA

--- get_tweet_data.py
+++ get_tweet_data.py
@@ -18,7 +18,7 @@ def get_all_tweets(screen_name):
     # max_number = 400
 
     min_date = datetime(2016, 4, 29, 0, 00)
-    max_date = datetime.today()
+    max_date = datetime.today() + datetime.timedelta(days=1) # to fix timezone difference between USA and Japan
 
     # min_date = datetime(2016, 4, 20, 0, 00)
     # max_date = datetime(2016, 4, 29, 0, 00)

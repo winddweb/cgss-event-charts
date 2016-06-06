a1214ca1df40281face63ae92038d7973657b614
Yifeng <winddweb@gmail.com>
2016-06-06 02:45:39 -0700

    Add get all command

--- get_borderline.py
+++ get_borderline.py
@@ -22,5 +22,14 @@ elif option == 'predict':
     output = get_tweet('cindere_border')
     print(output)
     draw_chart('cindere_border_tweets.csv')
+elif option == 'all':
+    # border
+    output = get_tweet('deresute_border')
+    print(output)
+    draw_chart('deresute_border_tweets.csv')
+    # predict
+    output = get_tweet('cindere_border')
+    print(output)
+    draw_chart('cindere_border_tweets.csv')
 else:
     print('Please give "predict" or "border" as the first argument')
